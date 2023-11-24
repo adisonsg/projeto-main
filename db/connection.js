e5426dc1a5dc94f813f9a0e01ac6e2dd17c8c990
juliocezargd <103080719+juliocezargd@users.noreply.github.com>
2023-11-23 23:41:22 -0300

    Update connection.js
    mysql

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,14 @@
 const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// Configuração para o PostgreSQL
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
+// Configuração para o MySQL
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql',
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+    // Adicione quaisquer opções específicas do MySQL, se necessário
+  },
 });
 
 module.exports = sequelize;
